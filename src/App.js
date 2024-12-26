bafa7bbff56b9335f2657a01ef74f7bd786e8c60
Fabiang120 <117304307+Fabiang120@users.noreply.github.com>
2024-12-25 21:11:15 -0500

    Added react logic

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Components/Home'
+function App() {
+  return(
+      <div>
+        <Home />
+      </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,10 @@
+function Home(){
+    return(
+        <div>
+            <h1>Welcome to My Website</h1>
+            <p>-Fabian Garcia</p>
+        </div>
+    );
+}
+
+export default Home;
