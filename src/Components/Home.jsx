593f0f723360e2153ceef5a4b14419f875797588
Fabiang120 <117304307+Fabiang120@users.noreply.github.com>
2024-12-29 17:02:50 -0500

    Added Resume parts

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -34,6 +34,67 @@ function Home(){
                 Amazon Store
             </a>
             </section>
+            <section className="about-me">
+                <h2>About Me</h2>
+                <p>
+                    A large part of my identity is rooted in my Cuban heritage and the sacrifices my parents made
+                    to provide me with the opportunities I have today. Their perseverance and dedication have always
+                    inspired me to work hard and strive for success.
+                </p>
+                <p>
+                    I am currently pursuing a Bachelor's degree in Computer Science at the University of Florida,
+                    where I combine my passion for technology with a drive to learn and grow. Alongside my studies,
+                    I am a businessman and the proud owner of Fabian's General Corner LLC, a wholesale company that
+                    serves customers across all 50 states.
+                </p>
+            </section>
+            <section className="Education">
+                <h3>Education</h3>
+                <p>
+                    University of Florida, Bachelor’s in Computer Science Expected Graduation: May 2026
+                    <p>Cumulative GPA: 3.40</p>
+                    <p>Relevant Coursework: Introduction to Computer Organization, Data Structures and Algorithms, Introduction to Software Engineering, Operating Systems, Machine Learning Fundamentals</p>
+                    <p>Involvement: Society of Software Developers & Society of Hispanic Professional Engineers</p>
+                    <p>Honors: Dean’s List (Fall 2022, Summer 2023, Spring 2024, Fall 2024)</p>
+                </p>
+            </section>
+            <section className="Work Experience">
+                <h4>Work Experience</h4>
+                <p>
+                    Owner of Fabian’s General Corner LLC March 2023- Present               
+                    <p>Retail Business on Amazon</p>
+                    <p> • Personally sourced and sold over $20,000 worth of products, managing over 2,000 individual sales </p>
+                    <p> • Directly negotiated with businesses and brands to secure deals and expand inventory</p>
+                    <p> • Expanded the business to operate successfully in all 50 states across the U.S</p> 
+                </p>
+            </section>
+            <section className="Projects">
+                <h5>Projects</h5>
+                <p>
+                    Sourcing Optimizer Application (C++) March 2024- Present               
+                    <p> • Extracts data from Excel sheets and uses Amazon’s API to compare market prices against filters</p>
+                    <p> • Implements multithreading, async HTTP requests, and cryptography with cpprest, OpenSSL, xlnt.</p>
+                    <p> • Developed a web-based file upload system using HTML, JavaScript, and a local server with CORS header validation</p>
+                </p>
+                <p>
+                    Criminal Facial Recognition Application (Python) February 2024 
+                    <p> • Developed a facial recognition system that enhances image resolution using machine learning</p>
+                    <p> • Utilized OpenCV and eigenvalue-based algorithms for image upscaling and facial recognition</p>
+                </p>
+                <p>
+                    US Camp Route Optimizer (3 team members, C++) December 2023
+                    <p> • Optimizes US camping route planning based on user preferences, integrating distance thresholds and amenities</p>
+                    <p> • Implements Dijkstra’s Algorithm and A* Search for efficient route calculation</p>
+                </p>
+            </section>
+            <section className="Skills">
+                <h6>Skills</h6>
+                <p>• Programming Languages: C++ (2 years), Python (3 years), Lua (1 year), C ({'>'}1 year), Java Script ({'>'}1 year), HTML ({'>'}1 year)</p>
+                <p>• Frameworks / Libraries: Google test, Fuse, CppRestSDK, OpenSSL, xlnt, Scikit-Learn, OpenCV, Keras, TensorFlow, Matplotlib.</p>
+                <p>• Tools and Databases: Amazon Seller Api, AWS Tools, Git, GitHub, Jupyter Notebook, Roblox Studio, Valgrind</p>
+                <p>• Languages: English (Native), Spanish (Native)</p>
+                <p>• Certifications: Microsoft Office Specialist: Word, Excel, and PowerPoint.</p>
+            </section>
         </div>
 
         // Display my Name bold //
